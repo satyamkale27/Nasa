3947fe61fda14b1c0f8b5e512f0ddbbc22100f42
satyam <satyamkale271@gmail.com>
2024-08-06 20:04:54 +0530

    module add

--- server/src/routes/planets/planets.routes.js
+++ server/src/routes/planets/planets.routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { getAllPlanets } = require("./planets.controllers");
 const planetRouter = express.Router();
 planetRouter.get("/planets", getAllPlanets);
 module.exports = planetRouter;
