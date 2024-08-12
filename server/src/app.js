445f8396998dbfe0c8646046f48c1f650cdf36f7
satyam <satyamkale271@gmail.com>
2024-08-12 18:51:22 +0530

    information logeed by morgan

--- server/src/app.js
+++ server/src/app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const path = require("path");
 const planetsRouter = require("./routes/planets/planets.routes");
 const cors = require("cors");
+const morgan = require("morgan");
 const app = express();
 app.use(
   cors({
     origin: "http://localhost:3000",
   })
 );
+app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(planetsRouter);
