fb6f7e73b31d79875dfd53b25ddd28a54072e3c7
satyam <satyamkale271@gmail.com>
2024-08-13 22:54:33 +0530

    passes to frontend to handel routing(matching)

--- server/src/app.js
+++ server/src/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(launchesRouter);
 app.use(planetsRouter);
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 module.exports = app;
