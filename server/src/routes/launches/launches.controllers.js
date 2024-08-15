4ad74aee8592e6889ee6f15082c9434385f1e368
satyam <satyamkale271@gmail.com>
2024-08-15 21:47:14 +0530

    addnewlaunch request

--- server/src/routes/launches/launches.controllers.js
+++ server/src/routes/launches/launches.controllers.js
@@ -1,8 +1,18 @@
-const { getAllLaunches } = require("../../models/launches.models");
+const {
+  getAllLaunches,
+  addNewLaunch,
+} = require("../../models/launches.models");
 function httpGetgetAllLaunches(req, res) {
   return res.status(200).json(getAllLaunches());
+}
+function httpAddNewLaunch(req, res) {
+  const launch = req.body;
+  launch.launchDate = new Date(launch.launchDate);
+  addNewLaunch(launch);
+  return res.status(201).json(launch);
 }
 
 module.exports = {
   httpGetgetAllLaunches,
+  httpAddNewLaunch,
 };
--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const { httpGetgetAllLaunches } = require("./launches.controllers");
+const {
+  httpGetgetAllLaunches,
+  httpAddNewLaunch,
+} = require("./launches.controllers");
 const launchesRouter = express.Router();
 launchesRouter.get("/launches", httpGetgetAllLaunches);
+launchesRouter.post("/launches", httpAddNewLaunch);
 module.exports = launchesRouter;
