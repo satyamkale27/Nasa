92958c48ad791828cd7bdf8a8612b3e87d5d161e
satyam <satyamkale271@gmail.com>
2024-08-17 23:27:28 +0530

    basic jest test

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,12 @@
+describe("Test GET /launches", () => {
+  test("it should respond with 200 success", () => {
+    const response = 200;
+    expect(response).toBe(200);
+  });
+});
+describe("Test POST /launch", () => {
+  test("it should respond with 200 success", () => {});
+
+  test("it should catch missing reqired property", () => {});
+  test("it should catch invalid dates", () => {});
+});
