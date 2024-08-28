4116063dea0c1154a95e90cc0fb423f184ca2d26
satyam <satyamkale271@gmail.com>
2024-08-28 14:07:15 +0530

    moongose and mongodb

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,28 @@
 const http = require("http");
+const mongoose = require("mongose");
 const app = require("./app");
 const PORT = process.env.PORT || 8000;
+const MONGO_URL =
+  "mongodb+srv://nasa-api:iBbJI1fIidT0lrUr@nasacluster.up1ge.mongodb.net/?retryWrites=true&w=majority&appName=NASACluster";
+
 const { loadPlanetData } = require("./models/planets.model");
 const server = http.createServer(app);
+
+mongoose.connection.once("error", (err) => {
+  console.error(err);
+});
+
+mongoose.connection.on("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTipology: true,
+  });
   await loadPlanetData();
   server.listen(PORT, () => {
     console.log(`Listening on ${PORT}...`);
