de8af6da95ff94d6a7acb9d505cc892444b45068
satyam <satyamkale271@gmail.com>
2024-09-10 22:01:28 +0530

    mongodb connection in seprate file

--- server/src/server.js
+++ server/src/server.js
@@ -1,23 +1,13 @@
 const http = require("http");
-const mongoose = require("mongoose");
 const app = require("./app");
+const { mongoConnect } = require("./services/mongo");
 const PORT = process.env.PORT || 8000;
-const MONGO_URL =
-  "mongodb+srv://nasa-api:iBbJI1fIidT0lrUr@nasacluster.up1ge.mongodb.net/?retryWrites=true&w=majority&appName=NASACluster";
 
 const { loadPlanetData } = require("./models/planets.model");
 const server = http.createServer(app);
 
-mongoose.connection.once("error", (err) => {
-  console.error(err);
-});
-
-mongoose.connection.on("open", () => {
-  console.log("MongoDB connection ready!");
-});
-
 async function startServer() {
-  await mongoose.connect(MONGO_URL);
+  await mongoConnect();
   await loadPlanetData();
   server.listen(PORT, () => {
     console.log(`Listening on ${PORT}...`);
---
+++ server/src/services/mongo.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const MONGO_URL =
+  "mongodb+srv://nasa-api:iBbJI1fIidT0lrUr@nasacluster.up1ge.mongodb.net/?retryWrites=true&w=majority&appName=NASACluster";
+
+mongoose.connection.on("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.once("error", (err) => {
+  console.error(err);
+});
+
+async function mongoConnect() {
+  await mongoose.connect(MONGO_URL);
+}
+
+module.exports = {
+  mongoConnect,
+};
